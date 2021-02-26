0715ab2357be1d92f54c90bfebb43fa6b40eccfa
Brandon Lile <brandon.lile@c2fo.com>
2021-02-26 17:33:10 +0000

    NestJS-Query project setup

---
+++ src/app.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TodoItemModule } from './todo-item/todo-item.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      database: 'gettingstarted',
+      username: 'gettingstarted',
+      port: 5488,
+      autoLoadEntities: true,
+      synchronize: true,
+      logging: true,
+    }),
+    GraphQLModule.forRoot({
+      // set to true to automatically generate schema
+      autoSchemaFile: true,
+    }),
+    TodoItemModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
